7c6d4258faa2f5aa0253a5d6f6ae0dc157cc69a0
BugorSA <vova9840@yandex.ru>
2020-09-01 19:33:17 +0400

    add Entity.Resume

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,19 +5,27 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    /**
-     * @Route("/1)
-     */
-    public function number()
-    {
-        $number = random_int(0, 100);
 
-        return $this->render('1.html.twig', [
-            'number' => $number,
-        ]);
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('interview/first.html.twig');
+    }
+
+    public function second(Request $request): Response
+    {
+        $name = $request->get("name");
+        $mail = $request->get("email");
+        $phone = $request->get("phone");
+
+        return new Response($name.' '.$mail.' '.$phone);
     }
 }
---
+++ src/Entity/Resume.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResumeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ResumeRepository::class)
+ */
+class Resume
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $telephone;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+}
