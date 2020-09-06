630c06e5d50c50e9e0f79f837bcdded7aa9d0521
BugorSA <vova9840@yandex.ru>
2020-09-06 21:45:19 +0400

    add validator in front

--- public/js/ajaxScript.js
+++ public/js/ajaxScript.js
@@ -1,4 +1,9 @@
 $('#send').click(function () {
+    var price = document.getElementsByName("price");
+    var provider = document.getElementsByName("provider")
+    var fran = document.getElementsByName("fran")
+    var comment = document.getElementsByName("comment");
+    var comment2 = document.getElementsByName("comment2");
     $.ajax({
         type: 'POST',
         url: '/second', //обращаемся к обработчику
@@ -7,7 +12,12 @@ $('#send').click(function () {
             email: $('#email').val(),
             phone: $('#phone').val(),
             area: $('#select').val(),
-            areaAn: $('#nameArea').val()
+            areaAn: $('#nameArea').val(),
+            price: price,
+            provider: provider,
+            fran: fran,
+            comment: comment,
+            comment2: comment2,
         },
         success: function (data) {  //в случае успеха выводим результаты в div "results"
             $('#formx').remove(); //скрываем форму после отправки
--- public/js/multiForm.js
+++ public/js/multiForm.js
@@ -1,72 +1,137 @@
 var currentTab = 0;
 showTab(currentTab);
 
+
 function showTab(n) {
     var x = document.getElementsByClassName("tab");
     x[n].style.display = "block";
-    if (n == 0) {
-        document.getElementById("prevBtn").style.display = "none";
+    if (n === 0) {
+        $('#prevBtn').hide(1);
     } else {
-        document.getElementById("prevBtn").style.display = "inline";
+        $('#prevBtn').show();
     }
-    if (n == (x.length - 1)) {
-        document.getElementById("nextBtn").innerHTML = "Submit";
+    if (n === (x.length - 1)) {
+        console.log("OK")
+        $('#submit_button').show(1);
+        $('#nextBtn').hide(1);
     } else {
-        document.getElementById("nextBtn").innerHTML = "Next";
+        $('#nextBtn').show(1);
+        $('#submit_button').hide(1);
     }
-    // ... and run a function that displays the correct step indicator:
-    fixStepIndicator(n)
 }
 
 function nextPrev(n) {
-    // This function will figure out which tab to display
     var x = document.getElementsByClassName("tab");
-    // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm()) return false;
-    // Hide the current tab:
+    if (n === 1 && !validate(currentTab)) return false;
     x[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
     currentTab = currentTab + n;
-    // if you have reached the end of the form... :
-    if (currentTab >= x.length) {
-        //...the form gets submitted:
-        document.getElementById("regForm").submit();
-        return false;
-    }
-    // Otherwise, display the correct tab:
     showTab(currentTab);
 }
 
-function validateForm() {
-    // This function deals with validation of the form fields
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].getElementsByTagName("input");
-    // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
-        // If a field is empty...
-        if (y[i].value == "") {
-            // add an "invalid" class to the field:
-            y[i].className += " invalid";
-            // and set the current valid status to false:
-            valid = false;
-        }
+function validate(currentTab) {
+    // var name = $('#name').val().toString();
+    // var email = $('#email').val().toString();
+    // var phone = $('#phone').val().toString();
+    // var select = $('#select').val().toString();
+    // var nameArea = $('#nameArea').val().toString();
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var select = document.getElementById("select");
+    var nameArea = document.getElementById("nameArea");
+    var price = document.getElementsByName("price");
+    var provider = document.getElementsByName("provider")
+    var fran = document.getElementsByName("fran")
+    var comment = document.getElementsByName("comment");
+    var comment2 = document.getElementsByName("comment2");
+    // var agree = document.getElementById("agree");
+    switch (currentTab) {
+        case 0:
+            let i = [];
+            if (!validateField(name.value)){
+                i.push(1)
+                name.className= "invalid"
+            }
+            if (!validateEmail(email.value)){
+                i.push(2)
+                i.push(email.value)
+                email.className= "invalid"
+            }
+            if (!validateField(phone.value)){
+                i.push(3)
+                phone.className= "invalid"
+            }
+            if (!validateField(select.value)){
+                i.push(4)
+                select.className= "invalid"
+            }
+            if (!validateArea(select.value,nameArea.value)){
+                i.push(5)
+                nameArea.className= "invalid"
+            }
+            console.log(i)
+            return (i.length === 0)
+        case 1:
+            let y = [];
+            if (!validateRadio(price)){
+                y.push(1)
+                price.className= "invalid"
+            }
+            if (!validateRadio(provider)){
+                y.push(2)
+                provider.className= "invalid"
+            }
+            if (!validateRadio(fran)){
+                y.push(3)
+                fran.className= "invalid"
+            }
+            console.log(y)
+            return (y.length === 0)
+        case 2:
+            let u = [];
+            if (!validateField(comment.item(0).value)){
+                u.push(1)
+                comment.className= "invalid"
+            }
+            if (!validateField(comment2.item(0).value)){
+                u.push(2)
+                comment2.className= "invalid"
+            }
+            // if (!agree.checked){
+            //     u.push(3)
+            //     agree.className= "invalid"
+            // }
+            console.log(u)
+            return (u.length === 0)
+        default:
+            return false;
     }
-    // If the valid status is true, mark the step as finished and valid:
-    if (valid) {
-        document.getElementsByClassName("step")[currentTab].className += " finish";
-    }
-    return valid; // return the valid status
 }
 
-function fixStepIndicator(n) {
-    // This function removes the "active" class of all steps...
-    var i, x = document.getElementsByClassName("step");
-    for (i = 0; i < x.length; i++) {
-        x[i].className = x[i].className.replace(" active", "");
-    }
-    //... and adds the "active" class to the current step:
-    x[n].className += " active";
+function validateEmail(email)
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
 
-$('#nameArea').click(function (){})
+function validateArea(area, nameArea) {
+    if (area === "Другое") {
+        return (nameArea !== "");
+    } else {
+        return true;
+    }
+}
+
+function validateField(field) {
+    return (field !== "")
+}
+
+function validateRadio(radios) {
+    var formValid = false;
+    var i = 0;
+    while (!formValid && i < radios.length) {
+        if (radios[i].checked) formValid = true;
+        i++;
+    }
+    return formValid;
+}
